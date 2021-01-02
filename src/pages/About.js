2ff0e75d81d537e4f8583675ca09d5b71ef9b870
broncs <jhowfrombrazil@gmail.com>
2021-01-02 11:12:55 -0300

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates,
+        esse! Expedita inventore quis accusantium laudantium repellendus
+        necessitatibus, deleniti pariatur corporis rerum quisquam consequuntur!
+        Voluptatem, nihil maiores, sed harum animi pariatur praesentium deleniti
+        nisi, tempora illo ratione. Corrupti neque nostrum ipsa?
+      </p>
+    </section>
+  );
+};
+
+export default About;
